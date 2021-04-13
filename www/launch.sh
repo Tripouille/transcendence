a9042ba4730a9c85f19e5697abc9fba21b32defa
Tripouille <jmichel.gambard@gmail.com>
2021-04-13 17:12:45 +0200

    add chown/chmod in launch.sh

--- www/launch.sh
+++ www/launch.sh
@@ -1,4 +1,6 @@
 su -c "pg_ctl start -D /var/lib/postgresql/data" - postgres
+chown -R postgres:postgres /var/lib/postgresql/data
+chmod 700 -R /var/lib/postgresql/data
 cd /www
 rm -rf /www/tmp/pids/server.pid
 /usr/bin/rails s -b '0.0.0.0' -p 80
