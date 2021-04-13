c91e2ce412d6ec5365aa653aa1bf09595fc83923
Tripouille <jmichel.gambard@gmail.com>
2021-04-13 17:09:25 +0200

    add bdd empty directory

--- www/launch.sh
+++ www/launch.sh
@@ -1,4 +1,4 @@
-#su -c "pg_ctl start -D /var/lib/postgresql/data" - postgres
+su -c "pg_ctl start -D /var/lib/postgresql/data" - postgres
 cd /www
 rm -rf /www/tmp/pids/server.pid
 /usr/bin/rails s -b '0.0.0.0' -p 80
