6b8140e32213474571c26f9da1b5758b5a613cdb
Tripouille <jmichel.gambard@gmail.com>
2021-04-19 10:19:21 +0200

    install webpacker

--- www/config/webpack/environment.js
+++ www/config/webpack/environment.js
@@ -1,11 +1,3 @@
 const { environment } = require('@rails/webpacker')
 
-const webpack = require('webpack')
-environment.plugins.append('Provide', new webpack.ProvidePlugin({
-	$: 'jquery',
-	jquery: 'jquery',
-	jQuery: 'jquery',
-	'window.jQuery': 'jquery'
-  }))
-
 module.exports = environment
