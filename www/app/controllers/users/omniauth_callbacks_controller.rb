a60fa2953be716e734fd143f1b047a8e0bee7571
Chinimala <astrid.allemandou@free.fr>
2021-06-23 02:21:44 +0200

    docker setup : precompile assets in all cases

--- www/app/controllers/users/omniauth_callbacks_controller.rb
+++ www/app/controllers/users/omniauth_callbacks_controller.rb
@@ -4,12 +4,15 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def marvin
     @user = User.from_omniauth(request.env["omniauth.auth"])
+	puts 'marvin'
     if @user.persisted?
+	  puts 'if'
       sign_in_and_redirect @user, event: :authentication
       set_flash_message(:notice, :success, kind: "42") if is_navigational_format?
       session[:user_id] = @user.id
 	  session[:user] = @user
     else
+	  puts 'else'
       session["devise.marvin_data"] = request.env["omniauth.auth"]
       redirect_to new_user_registration_url
     end
--- www/db/schema.rb
+++ www/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_20_172131) do
+ActiveRecord::Schema.define(version: 2021_06_23_000516) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,8 @@ ActiveRecord::Schema.define(version: 2021_06_20_172131) do
     t.string "login"
     t.string "provider"
     t.string "uid"
+    t.string "status"
+    t.datetime "last_activity"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["provider"], name: "index_users_on_provider"
     t.index ["uid"], name: "index_users_on_uid"
